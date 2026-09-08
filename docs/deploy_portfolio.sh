31e9e89beee41085814e8673e0f41bb0f187bd2f
FADM ECNM CORY ANDREW HOFSTAD USN <96269532+FADM-DCMN-CORY-A-HOFSTAD-USN@users.noreply.github.com>
2026-09-08 13:39:10 -0700

    Create deploy_portfolio.sh

---
+++ docs/deploy_portfolio.sh
@@ -0,0 +1,3 @@
+Aggiornamento dello Script di Deploy Automatico (`deploy_portfolio.sh`)
+
+Per assicurarci che questa calibrazione bioelettrica e tutti i paramenti strutturati finora siano compilati e protetti all'interno del ramo principale, aggiorniamo l'array dei file di staging dello script bash di automazione.
